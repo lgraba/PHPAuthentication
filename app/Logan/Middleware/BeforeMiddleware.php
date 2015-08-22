6c238300bc9244543df245913ec4581cdc98af01
lgraba <lgraba@gmail.com>
2015-08-21 23:28:25 -0600

    Finish up Logging Out

--- app/Logan/Middleware/BeforeMiddleware.php
+++ app/Logan/Middleware/BeforeMiddleware.php
@@ -13,7 +13,7 @@ class BeforeMiddleware extends Middleware
 
 	public function call()
 	{
-		// Before ever Slim application request, run a method
+		// Before every Slim application request, run a method
 		$this->app->hook('slim.before', [$this, 'run']);
 
 		// Send it on to the next call
@@ -31,6 +31,7 @@ class BeforeMiddleware extends Middleware
 		// Check Remember Me credentials (cookie : database) before every request to application
 		$this->checkRememberMe();
 
+		// Append data to view for usage by twig markup
 		$this->app->view()->appendData([
 			'auth' => $this->app->auth,
 			'baseUrl' => $this->app->config->get('app.url')
@@ -47,9 +48,37 @@ class BeforeMiddleware extends Middleware
 			// Separate cookie data by ___
 			$credentials = explode('___', $data);
 
-			// if (empty(trim($data)) || count($credentials) !== 2) {
-			// 	$this->app->response->redirect($this->app->urlFor('home'));
-			// }
+			if (empty(trim($data)) || count($credentials) !== 2) {
+				// This code sets up a redirect loop - this redirect will again trigger slim.before, recursively redirecting to home if the credential data is altered
+				// $this->app->response->redirect($this->app->urlFor('home'));
+			} else {
+				// Grab identifier credential from array
+				$identifier = $credentials[0];
+				// Grab token credential from array and hash it up
+				$token = $this->app->hash->hash($credentials[1]);
+
+				// Query database for user with matching remember_identifier
+				$user = $this->app->user
+					->where('remember_identifier', $identifier)
+					->first();
+
+				if ($user) {
+					// Check to see if our hashed cookie token matches the remember_token in database
+					if ($this->app->hash->hashCheck($token, $user->remember_token)) {
+
+						$this->app->flash('global', 'TEST');
+						// Log user in:
+						// Set SESSION to the user id
+						$_SESSION[$this->app->config->get('auth.session')] = $user->id;
+						// Set auth to the corresponding user
+						$this->app->auth = $this->app->user->where('id', $user->id)->first();
+
+					} else {
+						$user->removeRememberCredentials();
+					}
+				}
+
+			}
 		}
 	}
 
--- app/routes/auth/logout.php
+++ app/routes/auth/logout.php
@@ -8,6 +8,14 @@ $app->get('/logout', function () use ($app) {
 	// Unset SESSION 
 	unset($_SESSION[$app->config->get('auth.session')]);
 
+	// Check if cookie is set
+	if ($app->getCookie($app->config->get('auth.remember'))) {
+		// Remove Rember Me credentials for the user logging out
+		$app->auth->removeRememberCredentials();
+		// Delete cookie
+		$app->deleteCookie($app->config->get('auth.remember'));
+	}
+
 	// Flash logout message and redirect user to home
 	$app->flash('global', 'You are now logged out...');
 	$app->response->redirect($app->urlFor('home'));
